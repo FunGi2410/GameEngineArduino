cd2ac04813a12d6f746f7f28d04fc50558e7b06d
TamLC2512 <chitamle2512@gmail.com>
2025-08-20 22:27:12 +0700

    add cmd

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,9 @@ const char* ssid = "Nha Tro 7 Treo";
 const char* password = "02022025";
 
 void setup() {
+  // cmd
   Serial.begin(115200);
   setupOTA(ssid, password);
-  //sabjkfjgk
 }
 
 void loop() {
