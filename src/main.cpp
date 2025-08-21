702fa41184417623a047901b55fe202ed8f8c3ea
FunGi02 <nhan.nmt@enotion.io>
2025-08-21 23:57:20 +0700

    [HOST]: setup host to update code by espMDNS

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include "OTAWeb.h"
+#include "FuMDNS.h"
 
 const char* ssid = "Nha Tro 7 Treo";
 const char* password = "02022025";
@@ -7,6 +8,7 @@ const char* password = "02022025";
 void setup() {
   Serial.begin(115200);
   setupOTA(ssid, password);
+  setupHost();
 }
 
 void loop() {
